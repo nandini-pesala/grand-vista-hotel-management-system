b67a75ae613be20adc48a730ab9904cab6e5293b
Nandini Pesala <nandinipesala.tech@gmail.com>
2026-08-13 00:08:43 +0530

    Update CorsConfig.java

--- grand-vista-backend/src/main/java/com/grandvista/hotel/config/CorsConfig.java
+++ grand-vista-backend/src/main/java/com/grandvista/hotel/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("grand-vista-hotel-management-system.vercel.app")
+                        .allowedOrigins("https://grand-vista-hotel-management-system.vercel.app")
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
